bb44c0145191280f210e6b4de2ede3d22f813057
fengkx <liangkx8237@gmail.com>
2023-05-25 19:20:56 +0800

    fix: template

--- _templates/comp/new/web-entry.ejs.t
+++ _templates/comp/new/web-entry.ejs.t
@@ -2,7 +2,7 @@
 inject: true
 to: src/web/components/index.js
 skip_if: <%= compName %>
-before: __INSERT_BEFORE;
+before: '// __INSERT_BEFORE'
 ---
 
 export {default as <%= compName %>} from './<%= h.changeCase.paramCase(compName) %>';
--- src/web/components/index.js
+++ src/web/components/index.js
@@ -1,7 +1,4 @@
-import Button from './button';
-import Gantt from './gantt';
+export { default as Button } from './button';
+export { default as Gantt } from './gantt';
 
 // __INSERT_BEFORE
-
-// 为更好优化，web/components/index文件请不要使用export default，请使用export { Button }
-export { Button, Gantt };
