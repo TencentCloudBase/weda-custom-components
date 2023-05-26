baea3bcb5e38478b12ba1a3caa1caff392a0866c
fengkx <liangkx8237@gmail.com>
2023-05-26 14:52:25 +0800

    feat: slide

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,7 +2,11 @@ import Button from './components/button';
 import showToast from './actions/showToast';
 import Gantt from './components/gantt.json';
 
+import Slider from './components/slider.json';
+
 export const components = {
+  Slider,
+
   Button,
   Gantt,
 };
